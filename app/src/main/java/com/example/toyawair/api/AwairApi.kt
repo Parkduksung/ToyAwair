8e3b8ab72967f7974f755f9af416f8566b9a525b
Parkduksung <duksung1234@naver.com>
2021-10-01 00:43:02 +0900

    [refactor]AwairApi 수정. Call 리턴값 추가.

--- app/src/main/java/com/example/toyawair/api/AwairApi.kt
+++ app/src/main/java/com/example/toyawair/api/AwairApi.kt
@@ -1,5 +1,7 @@
 package com.example.toyawair.api
 
+import com.example.toyawair.api.response.AwairResponse
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -8,6 +10,6 @@ interface AwairApi {
     @GET("events")
     fun getEvents(
         @Query("next_page_token") nextPageToken: String = ""
-    )
+    ) : Call<AwairResponse>
 
 }
