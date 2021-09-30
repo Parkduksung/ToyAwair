727dbe24f937d6c5b5a79fbd811537c557062a77
Parkduksung <duksung1234@naver.com>
2021-10-01 02:34:05 +0900

    [feature] BaseTest 생성.

---
+++ app/src/test/java/base/BaseTest.kt
@@ -0,0 +1,21 @@
+package base
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.setMain
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+abstract class BaseTest {
+
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(TestCoroutineDispatcher())
+    }
+
+}
