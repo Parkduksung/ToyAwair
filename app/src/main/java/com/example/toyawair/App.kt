106bca97524106254c711ee03cfdc46707b13f78
Parkduksung <duksung1234@naver.com>
2021-10-01 16:13:45 +0900

    [feature] jakewharton dependency 적용

--- app/src/main/java/com/example/toyawair/App.kt
+++ app/src/main/java/com/example/toyawair/App.kt
@@ -1,11 +1,13 @@
 package com.example.toyawair
 
 import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        AndroidThreeTen.init(this);
     }
 }
